f5708c5adb1dd4b0e27ef3df4cdacf9022e4bbe8
Niko <soder.n@gmail.com>
2023-10-26 18:41:19 +0300

    Set tabs

--- init.lua
+++ init.lua
@@ -271,6 +271,12 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+-- Tabs
+vim.o.tabstop = 2 -- A TAB character looks like 2 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
