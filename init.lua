433627b27e02170703dfcb014d88c616c59fbfb6
Niko <soder.n@gmail.com>
2023-10-30 20:34:09 +0200

    Add relativenumbers and colorcolumn

--- init.lua
+++ init.lua
@@ -276,6 +276,8 @@ vim.o.tabstop = 2 -- A TAB character looks like 2 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+vim.opt.colorcolumn = '80'
+vim.opt.relativenumber = true
 
 -- [[ Basic Keymaps ]]
 
