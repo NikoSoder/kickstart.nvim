202d397d8677281cc1ac677bd3ded64d09995cc4
Niko <soder.n@gmail.com>
2024-03-19 16:30:11 +0200

    Comment

--- init.lua
+++ init.lua
@@ -675,9 +675,9 @@ require('lazy').setup {
       -- See `:help cmp`
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
-      luasnip.filetype_extend('javascript', { 'html' })
-      luasnip.filetype_extend('javascriptreact', { 'html' })
-      luasnip.filetype_extend('typescriptreact', { 'html' })
+      --luasnip.filetype_extend('javascript', { 'html' })
+      --luasnip.filetype_extend('javascriptreact', { 'html' })
+      --luasnip.filetype_extend('typescriptreact', { 'html' })
       require('luasnip.loaders.from_vscode').lazy_load()
       luasnip.config.setup {}
 
