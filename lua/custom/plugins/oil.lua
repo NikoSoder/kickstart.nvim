2d58d05a0368361ca069f39576f5b69aa1e488a4
Niko <soder.n@gmail.com>
2024-04-03 10:20:16 +0300

    Configure oil in float window

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,7 +1,18 @@
 return {
   'stevearc/oil.nvim',
-  opts = {},
+  opts = {
+    float = {
+      -- Padding around the floating window
+      padding = 2,
+      max_width = 60,
+      max_height = 16,
+      border = 'rounded',
+      win_options = {
+        winblend = 0,
+      },
+    },
+  },
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
 },
-  vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+  vim.keymap.set('n', '-', '<CMD>Oil --float<CR>', { desc = 'Open parent directory' })
