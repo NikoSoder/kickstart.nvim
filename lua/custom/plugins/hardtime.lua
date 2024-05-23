ed4516d61aa36f416cc5e9559da8adbc76c253de
Niko <soder.n@gmail.com>
2024-05-23 13:16:53 +0300

    Add neo-tree to hardtime ignore

--- lua/custom/plugins/hardtime.lua
+++ lua/custom/plugins/hardtime.lua
@@ -2,6 +2,6 @@ return {
   'm4xshen/hardtime.nvim',
   dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
   opts = {
-    disabled_filetypes = { 'qf', 'netrw', 'NvimTree', 'lazy', 'mason', 'oil' },
+    disabled_filetypes = { 'qf', 'netrw', 'NvimTree', 'lazy', 'mason', 'oil', 'neo-tree' },
   },
 }
