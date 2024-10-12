2a3d5fdd543de3330f2f0507e32052f973a60bb2
Niko <soder.n@gmail.com>
2024-10-12 18:26:50 +0300

    Clean up

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,18 +1,10 @@
 return {
   'stevearc/oil.nvim',
-  opts = {
-    float = {
-      -- Padding around the floating window
-      padding = 2,
-      max_width = 60,
-      max_height = 16,
-      border = 'rounded',
-      win_options = {
-        winblend = 0,
-      },
-    },
-  },
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
   -- Optional dependencies
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 },
-  vim.keymap.set('n', '-', '<CMD>Oil --float<CR>', { desc = 'Open parent directory' })
+  vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
